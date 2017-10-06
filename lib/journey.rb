d6fbab04ebe4e3cba2c7b47b874c6a2d56569ebb
Canace Wong <canacewongtoday@hotmail.com>
2017-10-06 15:32:15 +0100

    journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,9 +1,23 @@
 class Journey
   attr_reader :entry_station, :exit_station, :fare
 
-  def initialize
-    
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
+  def start(entry_station)
+    @entry_station = entry_station
   end
 
+  def finish(exit_station)
+    @exit_station = exit_station
+  end
+
+  def complete?
+    !!entry_station && !!exit_station
+  end
+
+  def calculate_fare
+    @fare = complete? ? MINIMUM_FARE : PENALTY_FARE
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -19,10 +19,43 @@ describe Journey do
     it "responds to method fare" do
       expect(journey).to respond_to(:fare)
     end
+
+    it 'returns entry station info upon entry' do
+      expect(journey.start(station)).to eq(station)
+    end
+
+    it 'returns exit station info upon exit' do
+      expect(journey.finish(station)).to eq(station)
+    end
+
+    it 'tells if a journey is completed' do
+      journey.start(station)
+      journey.finish(station)
+      expect(journey.complete?).to eq(true)
+    end
+
+    it 'tells if a journey is incompleted' do
+      journey.start(station)
+      expect(journey.complete?).to eq(false)
+    end
+
+    it 'tells if a journey is incompleted' do
+      journey.finish(station)
+      expect(journey.complete?).to eq(false)
+    end
+
+    it 'deducts fare accurately' do
+      allow(journey).to receive(:complete?).and_return(true)
+      expect(journey.calculate_fare).to eq(1)
+    end
+
+
+
+
 end
-  
 
-  
 
-  
+
+
+
 end
